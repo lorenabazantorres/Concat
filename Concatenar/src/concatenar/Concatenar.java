072ab2a6e1adb3fe25976cf0b3c2f86b50aff381
lorenabazantorres <lorenabazantorres@gmail.com>
2021-09-22 11:55:32 +0200

    Update Concatenar.java

--- Concatenar/src/concatenar/Concatenar.java
+++ Concatenar/src/concatenar/Concatenar.java
@@ -14,7 +14,9 @@ public class Concatenar {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+        String var1="Parte 1";
+        String var2="Parte 2";
+        System.out.println(var1+""+var2);
     }
     
 }
